656ab44494f1a14faeb972093088176d634c9a32
Jitesh817 <ydv.jitesh10@gmail.com>
2019-09-05 15:17:49 +0100

    inital commit

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,42 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("1", "spring", "spring description"),
+            new Topic("2", "java", "java description"),
+            new Topic("3", "c", "c description")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            if (topics.get(i).getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(topic -> topic.getId().equals(id));
+    }
+}
